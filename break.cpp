d08822903d02e97c1ad1f3feeb04da691004902d
Alexis Alameda <alamedaalexis@gmail.com>
2020-10-27 19:05:31 -0600

    Primera version de break.c

---
+++ break.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int i,n;
+	
+	printf("\n Este programa solo imprime numeros enteros \n");
+	
+	for(i=1;;i=i+1){
+		printf("\n Ingresa el numero entero \n");
+		scanf("%d",&n);
+		
+		if(n<0)
+			break;
+	
+		
+	}
+	
+	
+}
